57d6ed67fb5d7904637f4d9863127024fd25a96e
Ogunwole Isaac <tobiogunwole002@gmail.com>
2026-09-08 11:47:56 +0100

    Automate superuser creation in build pipeline

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically creates superuser if variables exist; skips silently if already created
+python manage.py createsuperuser --noinput || true
